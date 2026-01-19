e664d9e01e347badbe5706926ca4b4969ea31759
saraswati-ama <saraswati@aurahimalaya.org>
2026-01-19 14:30:45 +0545

    itinerary details implemented in view also

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: false,
-  experimental: {
-    turbo: {
-      root: process.cwd(),
-    },
-  },
   typescript: {
     ignoreBuildErrors: false,
   },
